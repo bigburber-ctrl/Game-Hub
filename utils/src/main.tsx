d74824e6a6b2751efb40c55b565dc74f7b56bf48
bigburber-ctrl <bigburber@gmail.com>
2026-02-15 17:27:47 -0500

    Fix Vercel CSS resolution for utils-root build

--- utils/src/main.tsx
+++ utils/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from '@/app/App'
-import '@/styles/index.css'
+import './styles/index.css'
 
 const container = document.getElementById('root')
 if (!container) throw new Error('Root container not found')
